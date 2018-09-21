edb810a7e8bc13f2884da7588b3903adabf6c7b6
Brian McCallister <brianm@skife.org>
2018-09-20 18:41:09 -0700

    format nicely

--- src/commands/index.rs
+++ src/commands/index.rs
@@ -1,9 +1,8 @@
 extern crate clap;
 use clap::ArgMatches;
 
-pub fn run_index(matches: &ArgMatches) {       
-     let index_path = matches.value_of("index").unwrap();
-        let geonames_path = matches.value_of("geonames").unwrap();
-        println!("indexing: index={} geonames={}", index_path, geonames_path);
-
+pub fn run_index(matches: &ArgMatches) {
+    let index_path = matches.value_of("index").unwrap();
+    let geonames_path = matches.value_of("geonames").unwrap();
+    println!("indexing: index={} geonames={}", index_path, geonames_path);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 extern crate clap;
 
-use clap::{Arg, App, SubCommand};
-use std::result;
+use clap::{App, Arg, SubCommand};
 use std::error::Error;
+use std::result;
 
 mod commands;
 
@@ -10,32 +10,36 @@ type Result<T> = result::Result<T, Box<Error>>;
 
 fn main() {
     let matches = App::new("World Time")
-                          .version(env!("CARGO_PKG_VERSION"))
-                          .author(env!("CARGO_PKG_AUTHORS"))
-                          .about(env!("CARGO_PKG_DESCRIPTION"))
-                          .subcommand(SubCommand::with_name("index")
-                            .about("rebuild the index of locations")
-                            .arg(Arg::with_name("geonames")
-                                    .short("g")
-                                    .long("geonames")
-                                    .takes_value(true)
-                                    .required(true)
-                                    .help("geonames primary database file"))
-                            .arg(Arg::with_name("index")
-                                    .short("i")
-                                    .long("index")
-                                    .takes_value(true)
-                                    .required(true)
-                                    .help("index file to produce")))
-                          .get_matches();
-    
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .subcommand(
+            SubCommand::with_name("index")
+                .about("rebuild the index of locations")
+                .arg(
+                    Arg::with_name("geonames")
+                        .short("g")
+                        .long("geonames")
+                        .takes_value(true)
+                        .required(true)
+                        .help("geonames primary database file"),
+                )
+                .arg(
+                    Arg::with_name("index")
+                        .short("i")
+                        .long("index")
+                        .takes_value(true)
+                        .required(true)
+                        .help("index file to produce"),
+                ),
+        )
+        .get_matches();
+
     let (subcommand, some_options) = matches.subcommand();
     let options = some_options.unwrap();
 
     match subcommand {
-        "index" => {
-            commands::run_index(options)
-        },
-        _ => panic!("unknown subcommand: {}", subcommand)
+        "index" => commands::run_index(options),
+        _ => panic!("unknown subcommand: {}", subcommand),
     };
 }
